bbe5c3dae0b610e130c15ed72ebb109ba33a6547
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-01-31 22:39:41 +0100

    Fix _is_outdated comparison

--- pacupdate.py
+++ pacupdate.py
@@ -247,7 +247,7 @@ class AURPackage:
             if resp is None:
                 return None
             else:
-                self._is_outdated = self.local_version == resp["Version"]
+                self._is_outdated = self.local_version != resp["Version"]
         return self._is_outdated
 
     async def get_rebuild_required(
